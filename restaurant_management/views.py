c11ea761d280e31661142aa47c9f1d9d697f277f
divyagowle7 <divyagowle7@gmail.com>
2025-11-11 12:45:57 +0530

    60350

--- restaurant_management/views.py
+++ restaurant_management/views.py
@@ -1,8 +1,14 @@
+#home/views.py
 from rest_framework import generics
 from .models import MenuCategory
 from .serializers import MenuCategorySerializer
 
 class MenuCategoryList(generics.ListAPIView):
+    """
+    API wndponit to retrive all menu categories.
+    """
+    #Define the queryset to retrive all MenyCategory instances
     queryset=MenuCategory.objects.all()
+    #Specify the serializer to use for MenuCategory instances
     serializer_class=MenuCategorySerializer
     

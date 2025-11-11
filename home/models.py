fcac0a00cf972e4eacb032d208e31fc226069609
divyagowle7 <divyagowle7@gmail.com>
2025-11-11 17:03:58 +0530

    60346

--- home/models.py
+++ home/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 class MenuCategory(models.Model):
-    nam
+    name =models.CharField(max_length=100,unique=True)
+
+    def __str__(self):
+        return self.name
+        

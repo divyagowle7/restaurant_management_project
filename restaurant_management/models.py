29f81bcb9714c5201a867f447f52ba593ca685bb
divyagowle7 <divyagowle7@gmail.com>
2025-11-11 12:59:21 +0530

    60350

--- restaurant_management/models.py
+++ restaurant_management/models.py
@@ -1,6 +1,15 @@
+# Import the models module from Django's database package
 from django.db import models
-class MenuCategory(models.Model):
-    name=models.CharField(max_length=100,unique=True)
 
-    def __str__(self):
-        return self.name
+# Define a model for menu categories
+class MenuCategory(models.Model):
+    # Field to store the name of the menu category
+    name=models.CharField(
+        max_length=100, # Maximum length of the category name
+        unique=True # Ensure category names are unique
+        )
+        
+        # Define a string representation of the model instance
+        def __str__(self):
+            # Return the name of the menu category as its string representation
+            return self.name

175547fca4db662965bd93f9c944aae21a98a771
divyagowle7 <divyagowle7@gmail.com>
2025-11-12 15:15:58 +0530

    60473

--- home/models.py
+++ home/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100,unique=True)
+    is_featured=models.BooleanField(default=False)
 
     def __str__(self):
         return self.name

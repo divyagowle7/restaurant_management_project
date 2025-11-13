401631592448c4a8b5a7ce180ccb76342b633c49
divyagowle7 <divyagowle7@gmail.com>
2025-11-13 12:29:37 +0530

    60545

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from home.models import MenuCategory
 # Create your models here.
 class Item(models.Model):
     item_name = models.CharField(max_length=150)
@@ -8,3 +8,14 @@ class Item(models.Model):
 
     def __str__(self):
         return str(self.item_name)
+
+class NutritionalInformation(models.Model):
+    menu_item=models.ForeignKey(MenuCategory,on_delete=models.CASCADE)
+    calories=models.IntegerField()
+    protien_grams=models.DecimalField(max_digits=5,decimal_places=2)
+    fat_grams=models.DecimalField(max_digits=5,decimal_places=2)
+    carbohydrate_grams=models.DecimalField(max_digits=5,decimal_places=2)
+
+    def __str__(self):
+        return f"{self.menu_item.name}:{self.calories} calories"
+        

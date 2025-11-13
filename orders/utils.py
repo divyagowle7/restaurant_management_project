d9038437c05f93e647862826cda15ef808dd5a7b
divyagowle7 <divyagowle7@gmail.com>
2025-11-13 11:58:50 +0530

    60471

--- orders/utils.py
+++ orders/utils.py
@@ -1,6 +1,8 @@
 import secrets
 import string
+import datetime
 from .models import Coupon # Assuming you have a coupon
+from DailyOperatingHours import DailyOperatingHours
 
 def generate_coupon_code(length=10):
     """
@@ -11,4 +13,12 @@ def generate_coupon_code(length=10):
         code=''.join(secrets.choice(characters) for _ in range(length))
         if not Coupon.objects.filter(code=code).exists():
             return code
+
+def get_today_operating_hours():
+    today=datetime.date.today().strftime('%A')
+    try:
+        hours=DailyOperatingHours.objects.get(day=today)
+        return (hours.open_time,hours.close_time)
+    except DailyOperatingHours.DoesNotExist:
+        return (None,None)
 

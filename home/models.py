1ae7ba537e11b8f2e7055338fdebc3a9450eb6d9
divyagowle7 <divyagowle7@gmail.com>
2025-11-14 10:45:04 +0530

    60542

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+import random
+from django.core.exceptions import ObjectDoesNotExist
 # Create your models here.
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100,unique=True)
@@ -7,4 +8,15 @@ class MenuCategory(models.Model):
 
     def __str__(self):
         return self.name
-        
+
+class DailySpecial(models.Model):
+    @staticmethod
+    def get_random_special():
+        try:
+            count=DailySpecial.objects.count()
+            if count==0:
+                return None
+            random_index=random.randint(0,count-1)
+            return DailySpecial.objects.all()[random_index]
+        except Exception as e:
+            raise Exception(f"Error fetching random daily special: {str(e)}")     

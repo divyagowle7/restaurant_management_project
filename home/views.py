85975b83c5f3e8a9a470434493c588899b01c723
divyagowle7 <divyagowle7@gmail.com>
2025-11-16 16:13:15 +0530

    60603

--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from rest_framework import generics,viewsets
+from rest_framework import generics,viewsets,status
 from .models import MenuCategory,MenuItem
 from .serializers import MenuCategorySerializer,MenuItemSerializer
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
 from django.db.models import Q
 # Create your views here.
 class MenuItemPagination(PageNumberPagination):
@@ -12,6 +14,7 @@ class MenuItemPagination(PageNumberPagination):
 class MenuCategoryList(generics.ListAPIView):
     queryset=MenuCategory.objects.filter(is_featured=True)
     serializer_class=MenuCategorySerializer
+
 class MenuItemIngredientsView(generics.RetrieveAPIView):
     queryset=MenuItem.objects.all()
     serializer_class=MenuItemSerializer
@@ -19,9 +22,22 @@ class MenuItemIngredientsView(generics.RetrieveAPIView):
     
 class MenuItemVieewSet(viewsets.ModelViewSet):
     queryset=MenuCategory.objects.all()
-    serializer_class=MenuCategorySerializer
+    serializer_class=MenuItemSerializer
     pagination_class=MenuItemPagination
+    permission_classes=[IsAdminUser]
 
+    def update(self,request,pk=None):
+        try:
+            menu_item=self.get_object()
+            serializer=self.get_seializer(menu_item,data=request.data,partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        except MenuItem.DoesNotExist:
+            return Response({'error':'Menu item not found'},status=status.HTTP_400_NOT_FOUND)
+
+            
     def get_queryset(self):
         queryset=super().get_queryset()
         search_query=self.request.query_params.get('search',None)

ca465640731e6fabd6f153306b482abaf1729cfa
divyagowle7 <divyagowle7@gmail.com>
2025-11-16 15:34:04 +0530

    60600

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import MenuCategoryList
+from . import views
 
 urlpatterns = [
-    path('appi/menu-categories-list/',MenuCategoryList.as_view()),
+    path('api/menu-categories-list/',MenuCategoryList.as_view()),
+    path('api/menu-items/<int:pk>/ingredients/',views.MenuItemIngredientsView.as_view()),
 ]

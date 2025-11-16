c307ff3e7e3e57da5139234d4bf755dfcf5852e2
divyagowle7 <divyagowle7@gmail.com>
2025-11-16 15:28:51 +0530

    60600

--- home/serializers.py
+++ home/serializers.py
@@ -1,6 +1,17 @@
 from rest_framework import serializers
-from .models import MenuCategory
+from .models import MenuCategory,MenuItem,Ingredient,MenuItemIngrediet
 
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Ingredient
+        fields=['id','name']
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    ingredients=IngredientSerializer(many=True,read_only=True)
+    class Meta:
+        model=MenuItem
+        fields=['id','name','ingredients']
+        
 class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=MenuCategory

9cb9caa7014edb648595e25fc1b8e08144bef3d9
divyagowle7 <divyagowle7@gmail.com>
2025-11-21 21:32:24 +0530

    60899

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import MenuCategoryList,MenuItemViewSet
+from .views import MenuCategoryList,MenuItemViewSet,TableDetailView
 from . import views
 
 router=DefaultRouter()
@@ -9,4 +9,5 @@ urlpatterns = [
     path('api/menu-categories-list/',MenuCategoryList.as_view()),
     path('api/menu-items/<int:pk>/ingredients/',views.MenuItemIngredientsView.as_view()),
     path('',include(router.urls)),
+    path('api/tables/<int:pk>/',TableDetailView.as_view(),name='table-detail'),
 ]

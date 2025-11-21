075443e581a19fc0de2fb3b6ce749d56080da79e
divyagowle7 <divyagowle7@gmail.com>
2025-11-21 19:00:27 +0530

    60901

--- orders/admin.py
+++ orders/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import Coupon
+from .models import Coupon,Order
+
 # Register your models here.
-admin.site.register(Coupon)
+def mark_orders_processed(modeladmin,request,queryset):
+    queryset.update(status='Processed')
+    mark_orders_processed.short_description='Mark selected orders as Processed'
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['id','status']
+    actions=[mark_orders_processed]
+
+admin.site.register(Coupon,Order,OrderAdmin)

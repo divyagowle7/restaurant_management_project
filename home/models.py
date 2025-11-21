70ef4ff5d008bd7f19c1e33867bf716806f615ba
divyagowle7 <divyagowle7@gmail.com>
2025-11-21 21:36:21 +0530

    60898

--- home/models.py
+++ home/models.py
@@ -10,6 +10,7 @@ class MenuCategory(models.Model):
     def __str__(self):
         return self.name
 class MenuItem(models.Model):
+    category=models.CharField(max_length=100)
     name=models.CharField(max_length=255)
     description=models.TextField()
     price=models.DecimalField(max_digits=10,decimal-places=2)

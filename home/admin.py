55ab49174c31474182ed2ac30dde50208335f7cc
divyagowle7 <divyagowle7@gmail.com>
2025-11-23 18:14:43 +0530

    61023

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Restaurant
 # Register your models here.
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display=('name','address','phone_number','email')
+    search_fields=('name','address')
+    list_filter=('is_active')
+admin.site.register(Restaurant,RestaurantAdmin)
+

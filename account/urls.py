6f59bbfbe2604fdd1fcd385c3d192df6fda58b2a
divyagowle7 <divyagowle7@gmail.com>
2025-11-29 15:07:59 +0530

    61293

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path,include
+from rest_framework import routers
 from .views import *
 
+router=routers.DefaultRouter()
+router.register(r'user-profile'.UserProfileViewSet,basename='user-profile')
+
 urlpatterns = [
-    
+    path('',include(router.urls)),
 ]

342ee4d6ccab9973b724653ee33b9578d8f18aff
divyagowle7 <divyagowle7@gmail.com>
2025-11-29 15:05:04 +0530

    61293

--- account/views.py
+++ account/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from .serializers import UserProfileSerializer
 # Create your views here.
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset=User.objects.all()
+    serializer_class=UserProfileSerializer
+    permission_class=[IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+    def update(self,request.*args,**kwargs):
+        partial=True
+        instance=self.get_object()
+        serializer=self.get_serializer(instance,data=request.data,partial=partial)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+        
+

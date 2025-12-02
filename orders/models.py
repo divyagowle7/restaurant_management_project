4c358d351a6f1ffafe7e55dd1737eff5911b24cf
divyagowle7 <divyagowle7@gmail.com>
2025-12-02 22:00:00 +0530

    61482

--- orders/models.py
+++ orders/models.py
@@ -49,5 +49,12 @@ class OrderItem(models.Model):
 
 class MenuItem(models.Model):
     name=models.CharField(mal=255)
-    
+
+class PaymenttMethod(models.Model):
+    name=models.CharField(max_length=50,unique=True)
+    description=models.TextField(blank=True,null=True)
+    is_active=models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name  
     

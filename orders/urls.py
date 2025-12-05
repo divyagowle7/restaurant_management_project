536cb8a2987d172bc102f5370a69fbebafb1821d
divyagowle7 <divyagowle7@gmail.com>
2025-12-05 21:50:58 +0530

    61612

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CouponValidationView,OrderHistoryView,OrderRetrieveView,PaymentMethodList
+from .views import CouponValidationView,OrderHistoryView,OrderRetrieveView,PaymentMethodList,OrderStatusUpdateView
 
 urlpatterns = [
     path('coupons/validate/',CouponValidationView.as_view(),name='coupon_validate'),
     path('order-history/',OrderHistoryView.as_view(),name='order-history'),
     path('orders/<int:order_id>/',OrderRetrieveView.as_view()),
     path('payment-methods/',PaymentMethodList.as_view()),
+    path('orders/<int:id>/status/',OrderStatusUpdateView.as_view(),name='update-order-status'),
 ]
